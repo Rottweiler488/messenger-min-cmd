94e6adcf41aa64d2bca185038270edf603c6ab3c
Rottweiler488 <rottweiler488@gmail.com>
2025-10-25 13:16:45 +0300

    create folders and run configs.

---
+++ client/src/main/java/com/rottweiler488/messengermin/MainClient.java
@@ -0,0 +1,9 @@
+package src.main.java.com.rottweiler488.messengermin;
+
+public class MainClient
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Start.");
+    }
+}
---
+++ server/src/main/java/com/rottweiler488/messengermin/model/User.java
@@ -0,0 +1,7 @@
+package src.main.java.com.rottweiler488.messengermin.model;
+
+public class User {
+    private String name;
+
+    public String name() { return name; }
+}
